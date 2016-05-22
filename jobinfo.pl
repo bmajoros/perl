4594f7cedbb1216b9f5c7ddde4b124b624f774c8
Bill Majoros <martiandna@gmail.com>
2016-05-22 08:46:04 -0400

    update

--- jobinfo.pl
+++ jobinfo.pl
@@ -7,17 +7,23 @@ die "$name <jobID>\n" unless @ARGV==1;
 my ($jobID)=@ARGV;
 
 open(IN,"sacct -j $jobID -l -p |") || die;
-my $header=<IN>; my $underlines=<IN>; my $data=<IN>;
-chomp $header; chomp $data;
-my @header=split/\|/,$header; my @data=split/\|/,$data;
+my $header=<IN>;
+#my $data=<IN>;
+chomp $header;
+my @header=split/\|/,$header;
 my $n=@header;
-print "$n attributes\n";
-for(my $i=0 ; $i<$n; ++$i) {
-  my $key=$header[$i];
-  my $value=$data[$i];
-  my $L=length($key);
-  for(my $i=$L ; $i<20 ; ++$i) { $key.=" " }
-  print "$key   $value\n";
+#print "$n attributes\n";
+while(<IN>) {
+  my $data=$_;
+  chomp $data;
+  my @data=split/\|/,$data;
+  for(my $i=0 ; $i<$n; ++$i) {
+    my $key=$header[$i];
+    my $value=$data[$i];
+    my $L=length($key);
+    for(my $i=$L ; $i<20 ; ++$i) { $key.=" " }
+    print "$key   $value\n";
+  }
 }
 close(IN);
 
